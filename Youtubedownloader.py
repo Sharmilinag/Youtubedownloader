7833fe702a87a286e9d6fe4dcda96f5db4d28dd8
Sharmili Nag <sn469@njit.edu>
2016-11-18 02:48:21 -0500

    Add files via upload

---
+++ Youtubedownloader.py
@@ -0,0 +1,11 @@
+#api
+#tkinkter
+#scikit
+#http://www.bogotobogo.com/VideoStreaming/YouTube/youtube-dl-embedding.php
+from __future__ import unicode_literals
+import youtube_dl
+import urllib
+import shutil
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=PT2_F-1esPk'])
